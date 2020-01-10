74d6b15ed75de7bdbc6ef862d7b8edeb41433642
cfn <cfn@leapy.cn>
2020-01-10 18:19:27 +0800

    '后台模板'

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -8,5 +8,12 @@ use app\admin\BaseController;
 
 class Index extends BaseController
 {
-
+    /**
+     * 后台首页
+     * @return \think\response\View
+     */
+    public function index()
+    {
+        return view();
+    }
 }
