a7a12d4424b080e2e722c78a858cfb7da59ba92d
cfn <cfn@leapy.cn>
2020-01-15 19:33:38 +0800

    '定时器'

--- learn/workerman/admin/WorkerService.php
+++ learn/workerman/admin/WorkerService.php
@@ -112,10 +112,9 @@ class WorkerService extends Server
     public function onMessage(TcpConnection $connection, $res)
     {
         $connection->lastMessageTime = time();
-        $res = json_decode($res, true);
         var_dump($res);
+        $res = json_decode($res, true);
         if (!$res || !isset($res['type']) || !$res['type'] || $res['type'] == 'ping') return;
-        var_dump('onMessage', $res);
         if (!method_exists($this->handle, $res['type'])) return;
         $this->handle->{$res['type']}($connection, $res + ['data' => []], $this->response->connection($connection));
     }
