804722aa17fabbce170f6660eae344352d52ed8c
cfn <cfn@leapy.cn>
2020-01-16 10:41:49 +0800

    'ws'

--- learn/workerman/channel/ChannelService.php
+++ learn/workerman/channel/ChannelService.php
@@ -8,7 +8,7 @@ use Channel\Server;
 use Workerman\Connection\TcpConnection;
 use Workerman\Worker;
 
-class ChannelService extends Server
+class ChannelService extends \think\worker\Server
 {
 
     /**
