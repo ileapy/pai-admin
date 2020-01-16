f685c5b75a16b7395954610781f39d64f4689ae9
cfn <cfn@leapy.cn>
2020-01-16 19:27:32 +0800

    'channer'

--- learn/workerman/channel/ChannelService.php
+++ learn/workerman/channel/ChannelService.php
@@ -56,7 +56,7 @@ class ChannelService extends Server
     {
         parent::init();
         $this->_worker = $this->worker;
-        $channel_server = new \Channel\Server('0.0.0.0', 2206);
+        parent::start();
     }
 
     /**
