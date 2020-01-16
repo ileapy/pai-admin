a9a72007934dfdab55a6b145aa6723024a1a333f
cfn <cfn@leapy.cn>
2020-01-16 21:50:18 +0800

    'ws'

--- learn/workerman/admin/WorkerHandle.php
+++ learn/workerman/admin/WorkerHandle.php
@@ -39,8 +39,7 @@ class WorkerHandle
 //
 //        $connection->adminInfo = $session->get('adminInfo');
 //        $connection->sessionId = $sessionId;
-        $sessionId['id'] = 1;
-        $connection->adminInfo = [];
+        $connection->adminInfo = ['id'=>1];
         $connection->sessionId = $sessionId;
         $this->service->setUser($connection);
 
