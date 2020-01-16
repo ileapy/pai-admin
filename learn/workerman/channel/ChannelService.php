d12d4eb5f502b062d4b892cffe87eb50d60c5ea6
cfn <cfn@leapy.cn>
2020-01-16 21:03:37 +0800

    'channer'

--- learn/workerman/channel/ChannelService.php
+++ learn/workerman/channel/ChannelService.php
@@ -122,6 +122,7 @@ class ChannelService extends Server
                 }
                 break;
             case 'publish':
+                var_dump($worker->channels);
                 foreach($channels as $channel)
                 {
                     if(empty($worker->channels[$channel]))
