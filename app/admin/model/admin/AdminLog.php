c0917c93fb98f46c486c0eb2bf47229065092f94
cfn <cfn@leapy.cn>
2020-01-17 20:13:58 +0800

    'ws'

---
+++ app/admin/model/admin/AdminLog.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\admin\model\admin;
+
+
+use app\admin\model\BaseModel;
+
+/**
+ * 操作日志
+ * Class AdminLog
+ * @package app\admin\model\admin
+ */
+class AdminLog extends BaseModel
+{
+
+}
