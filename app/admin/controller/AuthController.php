471694e73634ac8c17edff22bf7a81066004d9c1
cfn <cfn@leapy.cn>
2020-01-19 14:33:03 +0800

    '权限'

--- app/admin/controller/AuthController.php
+++ app/admin/controller/AuthController.php
@@ -95,7 +95,6 @@ abstract class AuthController extends BaseController
         if (in_array($this->action,$this->noNeedLogin) || $this->noNeedLogin == ['*'] || $this->noNeedLogin == "*") return true;
         // 验证登录
         if (!self::isActive()) return $this->redirect($this->request->domain().'/admin/login/login');
-        var_dump($this->adminInfo);
         $this->auth = AdminRole::getAuth($this->adminInfo['role_id']);
         var_dump($this->auth);
         // 权限验证
