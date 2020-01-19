29fb93115a39041a74ce0fa5085dfdc2ae77af7d
cfn <cfn@leapy.cn>
2020-01-19 11:02:18 +0800

    '日志记录'

---
+++ app/admin/controller/admin/AdminLog.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace app\admin\controller\admin;
+
+
+use app\admin\BaseController;
+
+class AdminLog extends BaseController
+{
+    public function index()
+    {
+        var_dump(123);
+    }
+}
