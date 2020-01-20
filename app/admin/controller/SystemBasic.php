b1474e8cd38b40cc0d79aaf75307feabed391be1
cfn <cfn@leapy.cn>
2020-01-20 10:27:59 +0800

    'session'

--- app/admin/controller/SystemBasic.php
+++ app/admin/controller/SystemBasic.php
@@ -20,6 +20,7 @@ class SystemBasic extends BaseController
      * @param string $title 标题
      * @param int $duration 持续时间
      * @return mixed
+     * @throws \Exception
      */
     protected function failedNotice($msg = '操作失败', $backUrl = 0, $info = '', $duration = 3)
     {
@@ -34,6 +35,7 @@ class SystemBasic extends BaseController
      * @param int $backUrl
      * @param string $title
      * @return mixed
+     * @throws \Exception
      */
     protected function failedNoticeLast($msg = '操作失败', $backUrl = 0, $info = '')
     {
@@ -47,6 +49,7 @@ class SystemBasic extends BaseController
      * @param string $title 标题
      * @param int $duration 持续时间
      * @return mixed
+     * @throws \Exception
      */
     protected function successfulNotice($msg = '操作成功',$backUrl = 0,$info = '',$duration = 3)
     {
@@ -61,6 +64,7 @@ class SystemBasic extends BaseController
      * @param int $backUrl
      * @param string $title
      * @return mixed
+     * @throws \Exception
      */
     protected function successfulNoticeLast($msg = '操作成功',$backUrl = 0,$info = '')
     {
@@ -71,6 +75,7 @@ class SystemBasic extends BaseController
      * 错误提醒页面
      * @param string $msg
      * @param int $url
+     * @throws \Exception
      */
     protected function failed($msg = '哎呀…亲…您访问的页面出现错误', $url = 0)
     {
@@ -86,6 +91,7 @@ class SystemBasic extends BaseController
      * 成功提醒页面
      * @param string $msg
      * @param int $url
+     * @throws \Exception
      */
     protected function successful($msg, $url = 0)
     {
@@ -96,8 +102,10 @@ class SystemBasic extends BaseController
             exit($this->fetch('public/success'));
         }
     }
+
     /**异常抛出
      * @param $name
+     * @throws \Exception
      */
     protected function exception($msg = '无法打开页面')
     {
@@ -107,6 +115,7 @@ class SystemBasic extends BaseController
 
     /**找不到页面
      * @param $name
+     * @throws \Exception
      */
     public function _empty($name)
     {
