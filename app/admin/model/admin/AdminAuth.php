14de9f7e945a402cd3db65338f2b8b734725d775
cfn <cfn@leapy.cn>
2020-01-25 17:04:48 +0800

    '权限管理'

--- app/admin/model/admin/AdminAuth.php
+++ app/admin/model/admin/AdminAuth.php
@@ -38,7 +38,7 @@ class AdminAuth extends BaseModel
         $model = $model->where("is_menu",1);
         $model = $model->where("pid",0);
         $model = $model->field(['name as title','path as href','icon']);
-        $model = $model->order(["rank desc","id desc"]);
+        $model = $model->order(["rank desc","id"]);
         $data = $model->select()->each(function ($item)
         {
 //            var_dump($item);
