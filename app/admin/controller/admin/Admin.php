ba933144e5960fccbfc8a0e4c853eca17d6305bf
cfn <cfn@leapy.cn>
2020-01-28 13:44:43 +0800

    '添加账号'

--- app/admin/controller/admin/Admin.php
+++ app/admin/controller/admin/Admin.php
@@ -59,9 +59,9 @@ class Admin extends AuthController
     public function add(Request $request)
     {
         $form = array();
-        $form[] = Elm::input('order_id','订单编号');
-        $form[] = Elm::number('total_price','商品总价')->min(0);
-        $form[] = Elm::number('total_postage','原始邮费')->min(0);
+        $form[] = Elm::input('order_id','订单编号')->col(8);
+        $form[] = Elm::number('total_price','商品总价')->col(8)->min(0);
+        $form[] = Elm::number('total_postage','原始邮费')->col(8)->min(0);
         return Form::make_post_form($form, url('save')->build());
 //        $this->assign("html", Form::make_post_form($form, url('save')->build()));
 //        return $this->fetch("public/form-builder");
