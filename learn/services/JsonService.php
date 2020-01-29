bb943b916e141533bd11f0ef6bcfd7bc8091d29e
cfn <cfn@leapy.cn>
2020-01-29 17:42:11 +0800

    '账号管理'

--- learn/services/JsonService.php
+++ learn/services/JsonService.php
@@ -58,14 +58,13 @@ class JsonService
         return self::result($code,$msg,$data);
     }
 
-    public static function success($msg,$data=[],$code=200)
+    public static function success($msg,$data=[])
     {
         if(true == is_array($msg)){
             $data = $msg;
             $msg = self::$SUCCESSFUL_DEFAULT_MSG;
         }
-        var_dump($code);
-        return self::result($code,$msg,$data);
+        return self::result(200,$msg,$data);
     }
 
     /*
