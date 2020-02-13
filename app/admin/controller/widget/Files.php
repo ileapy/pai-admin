271f66643b7fdf345d9100cad3f48ab8b75316a6
cfn <cfn@leapy.cn>
2020-02-13 23:10:22 +0800

    iframe

--- app/admin/controller/widget/Files.php
+++ app/admin/controller/widget/Files.php
@@ -15,6 +15,6 @@ class Files extends AuthController
     public function image()
     {
         $savename = \think\facade\Filesystem::putFile( 'image', request()->file('file'));
-        return $savename ? app("json")->success("上传成功",$savename) : app("json")->fail("上传失败");
+        return $savename ? app("json")->success("上传成功",['path'=>$savename]) : app("json")->fail("上传失败");
     }
 }
