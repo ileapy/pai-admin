085c676a2ee8d30771eac7c373e5b7d9345dc1a9
cfn <cfn@leapy.cn>
2020-02-14 11:31:23 +0800

    iframe

--- app/admin/model/admin/AdminAuth.php
+++ app/admin/model/admin/AdminAuth.php
@@ -138,5 +138,6 @@ class AdminAuth extends BaseModel
         elseif ($num > 1) for($i=0;$i<$num;$i++)
             if ($i==0) $str .= "|--";
             else $str .= "--";
+        return $str;
     }
 }
