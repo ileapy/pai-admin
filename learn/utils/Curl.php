2a49958c851c15d35304a8669a31504cbd1709a8
cfn <cfn@leapy.cn>
2020-02-15 18:32:45 +0800

    拼多多

--- learn/utils/Curl.php
+++ learn/utils/Curl.php
@@ -82,6 +82,7 @@ class Curl
     public function post()
     {
         var_dump($this->params);
+        var_dump($this->header);
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $this->url);
         curl_setopt($curl, CURLOPT_HEADER, 0);
