2f8fbaabbe1d6c3f14ce5b181054c0e54c2c513a
cfn <cfn@leapy.cn>
2020-02-15 17:44:55 +0800

    拼多多

--- app/admin/controller/pinduoduo/Authorization.php
+++ app/admin/controller/pinduoduo/Authorization.php
@@ -6,6 +6,8 @@ namespace app\admin\controller\pinduoduo;
 
 use app\admin\controller\AuthController;
 use app\Request;
+use learn\services\JsonService as Json;
+use learn\services\UtilService as Util;
 
 /**
  * 权限
@@ -17,9 +19,12 @@ class Authorization extends AuthController
     /**
      * 验证返回code
      * @param Request $request
+     * @return string
      */
     public function accessauth(Request $request)
     {
-        var_dump($request);
+        $data = Util::getMore([['code',''],['state',0]]);
+        if ($data['code'] == "" || $data['state'] == 0) return "授权环境异常";
+        var_dump($data);
     }
 }
