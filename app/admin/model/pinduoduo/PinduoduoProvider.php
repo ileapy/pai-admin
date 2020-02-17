40b6296537fef71e712dbc872a6a941546eae00f
cfn <cfn@leapy.cn>
2020-02-17 15:53:05 +0800

    拼多多

--- app/admin/model/pinduoduo/PinduoduoProvider.php
+++ app/admin/model/pinduoduo/PinduoduoProvider.php
@@ -53,7 +53,7 @@ class PinduoduoProvider extends BaseModel
     {
         $model = new self;
         $model = $model->where("status",1);
-        $model = $model->where("use_num","<","limit_num");
+        $model = $model->where("use_num","<",5);
         $info = $model->find();
         return $info ? $info->toArray() : [];
     }
