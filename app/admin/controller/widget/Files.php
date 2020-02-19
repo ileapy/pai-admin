56edd21b0233798c71621b3f09b988b7c0f731a1
cfn <cfn@leapy.cn>
2020-02-19 15:31:50 +0800

    拼多多

--- app/admin/controller/widget/Files.php
+++ app/admin/controller/widget/Files.php
@@ -6,6 +6,7 @@ namespace app\admin\controller\widget;
 
 use app\admin\controller\AuthController;
 use learn\services\JsonService as Json;
+use learn\services\UtilService as Util;
 
 class Files extends AuthController
 {
@@ -17,5 +18,25 @@ class Files extends AuthController
     {
         $savename = \think\facade\Filesystem::putFile( 'image', request()->file('file'));
         return $savename ? Json::success("上传成功",['filePath'=>"/upload/".$savename,"name"=>$savename]) : app("json")->fail("上传失败");
+    }
+
+    /**
+     * base64转image
+     * @return mixed
+     */
+    public function baseToImage()
+    {
+        $data = Util::postMore([
+            ['image','']
+        ]);
+        if ($data['image'] == '') return app("json")->fail("上传失败,没有文件");
+        $path = "/upload/image/".date("Ymd");
+        if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $data['image'], $result)){
+            $type = $result[2];
+            if(!file_exists($path)) mkdir($path, 0700);
+            $savename = $path.'/'.md5(time()).".{$type}";
+            if (file_put_contents($savename, base64_decode(str_replace($result[1], '', $data['image'])))) return app("json")->success("上传成功",['src'=>$savename]);
+            else return app("json")->fail("上传失败，写入文件失败！");
+        }else return app("json")->fail("上传失败,图片格式有误！");
     }
 }
