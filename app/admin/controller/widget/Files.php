8ecd31e93198bea2c0ac80e14219d1eac5873b8c
cfn <cfn@leapy.cn>
2020-02-19 15:35:19 +0800

    拼多多

--- app/admin/controller/widget/Files.php
+++ app/admin/controller/widget/Files.php
@@ -30,11 +30,11 @@ class Files extends AuthController
             ['image','']
         ]);
         if ($data['image'] == '') return app("json")->fail("上传失败,没有文件");
-        $path = "/upload/image/".date("Ymd");
+        $path = "/upload/image/".date("Ymd").'/';
         if (preg_match('/^(data:\s*image\/(\w+);base64,)/', $data['image'], $result)){
             $type = $result[2];
             if(!file_exists($path)) mkdir($path, 0700);
-            $savename = $path.'/'.md5(time()).".{$type}";
+            $savename = $path.md5(time()).".{$type}";
             if (file_put_contents($savename, base64_decode(str_replace($result[1], '', $data['image'])))) return app("json")->success("上传成功",['src'=>$savename]);
             else return app("json")->fail("上传失败，写入文件失败！");
         }else return app("json")->fail("上传失败,图片格式有误！");
