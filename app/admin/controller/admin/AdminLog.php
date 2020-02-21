986ff8c099d036164840d90b87a97957f35012f3
cfn <cfn@leapy.cn>
2020-02-21 10:24:02 +0800

    通知

--- app/admin/controller/admin/AdminLog.php
+++ app/admin/controller/admin/AdminLog.php
@@ -6,12 +6,17 @@ namespace app\admin\controller\admin;
 
 use app\admin\controller\AuthController;
 
+/**
+ * 日志
+ * Class AdminLog
+ * @package app\admin\controller\admin
+ */
 class AdminLog extends AuthController
 {
     protected $noNeedLogin = [];
 
     public function index()
     {
-        var_dump(123);
+        return $this->fetch();
     }
 }
