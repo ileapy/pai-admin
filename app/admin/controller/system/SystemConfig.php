f47f156ab48dbaf13fffe4cd574177c7cd81ab6c
cfn <cfn@leapy.cn>
2020-02-21 14:21:07 +0800

    通知

--- app/admin/controller/system/SystemConfig.php
+++ app/admin/controller/system/SystemConfig.php
@@ -6,7 +6,15 @@ namespace app\admin\controller\system;
 
 use app\admin\controller\AuthController;
 
+/**
+ * 系统配置
+ * Class SystemConfig
+ * @package app\admin\controller\system
+ */
 class SystemConfig extends AuthController
 {
-
+    public function index()
+    {
+        return $this->fetch();
+    }
 }
---
+++ app/admin/model/system/SystemConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace app\admin\model\system;
+
+
+use app\admin\model\BaseModel;
+use app\admin\model\ModelTrait;
+
+/**
+ * 系统配置
+ * Class SystemConfig
+ * @package app\admin\model\system
+ */
+class SystemConfig extends BaseModel
+{
+    use ModelTrait;
+}
