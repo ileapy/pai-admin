c73d90fb70e2720524d1aa57fae1877cd1a08c3d
cfn <cfn@leapy.cn>
2020-02-21 14:08:18 +0800

    通知

---
+++ app/admin/controller/TemplateBasic.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace app\admin\controller;
+
+
+class TemplateBasic extends SystemBasic
+{
+
+}
---
+++ app/admin/controller/system/SystemConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace app\admin\controller\system;
+
+
+use app\admin\controller\AuthController;
+
+class SystemConfig extends AuthController
+{
+
+}
