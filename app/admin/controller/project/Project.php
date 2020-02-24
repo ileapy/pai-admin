1cb469059d3c355fcdc46275c6d774fb5fb7fdb9
cfn <cfn@leapy.cn>
2020-02-24 16:04:24 +0800

    项目

--- app/admin/controller/project/Project.php
+++ app/admin/controller/project/Project.php
@@ -53,17 +53,18 @@ class project extends AuthController
             $list = aModel::lst();
             $menus=[];
             foreach ($list as $menu){
-                $menus[] = ['value'=>$menu['id'],'label'=>$menu['realname']];//,'disabled'=>$menu['pid']== 0];
+                $menus[] = ['value'=>$menu['id'],'label'=>$menu['id']." - ".$menu['realname']];//,'disabled'=>$menu['pid']== 0];
             }
             return $menus;
         })->col(10);
-        $form[] = Elm::input('nickname','')->col(10);
-        $form[] = Elm::password('pwd','密码')->col(10);
-        $form[] = Elm::input('realname','真实姓名')->col(10);
-
-        $form[] = Elm::input('tel','电话')->col(10);
-        $form[] = Elm::email('mail','邮箱')->col(10);
-        $form[] = Elm::radio('status','状态',1)->options([['label'=>'启用','value'=>1],['label'=>'冻结','value'=>0]])->col(10);
+        $form[] = Elm::textarea('intro','项目简介')->col(24);
+        $form[] = Elm::password('start_time','开始时间')->col(10);
+        $form[] = Elm::input('end_time','结束时间')->col(10);
+        $form[] = Elm::textarea('intro','项目备注')->col(24);
+        $form[] = Elm::input('sql_ip','数据库IP')->col(10);
+        $form[] = Elm::email('sql_name','数据库账号')->col(10);
+        $form[] = Elm::input('sql_password','数据库密码')->col(10);
+        $form[] = Elm::radio('status','状态',0)->options([['label'=>'未开始','value'=>0],['label'=>'进行中','value'=>1],['label'=>'已完成','value'=>2],['label'=>'维护中','value'=>3],['label'=>'以终止','value'=>4]])->col(10);
         return Form::make_post_form($form, url('save')->build());
     }
 }
