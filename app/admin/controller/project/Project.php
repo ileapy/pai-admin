c3ed0029d397536350badd11484569730f983c29
cfn <cfn@leapy.cn>
2020-02-24 16:08:49 +0800

    项目

--- app/admin/controller/project/Project.php
+++ app/admin/controller/project/Project.php
@@ -58,13 +58,43 @@ class project extends AuthController
             return $menus;
         })->col(10);
         $form[] = Elm::textarea('intro','项目简介')->col(24);
-        $form[] = Elm::password('start_time','开始时间')->col(10);
-        $form[] = Elm::input('end_time','结束时间')->col(10);
+        $form[] = Elm::dateTimeRange('start_time','开始时间')->col(10);
         $form[] = Elm::textarea('intro','项目备注')->col(24);
         $form[] = Elm::input('sql_ip','数据库IP')->col(10);
-        $form[] = Elm::email('sql_name','数据库账号')->col(10);
-        $form[] = Elm::input('sql_password','数据库密码')->col(10);
+        $form[] = Elm::input('sql_name','数据库账号')->col(10);
+        $form[] = Elm::password('sql_password','数据库密码')->col(10);
         $form[] = Elm::radio('status','状态',0)->options([['label'=>'未开始','value'=>0],['label'=>'进行中','value'=>1],['label'=>'已完成','value'=>2],['label'=>'维护中','value'=>3],['label'=>'以终止','value'=>4]])->col(10);
+        return Form::make_post_form($form, url('save')->build());
+    }
+
+    /**
+     * 修改
+     * @param Request $request
+     * @return string
+     * @throws \FormBuilder\Exception\FormBuilderException
+     */
+    public function edit($id='')
+    {
+        if (!$id) return app("json")->fail("项目id不能为空");
+        $info = pModel::get($id);
+        if (!$info) return app("json")->fail("没有该项目");
+        $form = array();
+        $form[] = Elm::input('name','项目名称',$info['name'])->col(10);
+        $form[] = Elm::select('manager','项目负责人',$info['manager'])->options(function(){
+            $list = aModel::lst();
+            $menus=[];
+            foreach ($list as $menu){
+                $menus[] = ['value'=>$menu['id'],'label'=>$menu['id']." - ".$menu['realname']];//,'disabled'=>$menu['pid']== 0];
+            }
+            return $menus;
+        })->col(10);
+        $form[] = Elm::textarea('intro','项目简介',$info['intro'])->col(24);
+        $form[] = Elm::dateTimeRange('start_time','开始时间',$info['start_time'],$info['end_time'])->col(10);
+        $form[] = Elm::textarea('intro','项目备注',$info['intro'])->col(24);
+        $form[] = Elm::input('sql_ip','数据库IP',$info['sql_ip'])->col(10);
+        $form[] = Elm::input('sql_name','数据库账号',$info['sql_name'])->col(10);
+        $form[] = Elm::password('sql_password','数据库密码',$info['sql_password'])->col(10);
+        $form[] = Elm::radio('status','状态',$info['status'])->options([['label'=>'未开始','value'=>0],['label'=>'进行中','value'=>1],['label'=>'已完成','value'=>2],['label'=>'维护中','value'=>3],['label'=>'以终止','value'=>4]])->col(10);
         return Form::make_post_form($form, url('save')->build());
     }
 }
