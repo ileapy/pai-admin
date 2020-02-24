514b45e8c4a552b42f05726365b71411acc32169
cfn <cfn@leapy.cn>
2020-02-24 16:23:10 +0800

    项目

--- app/admin/model/project/project.php
+++ app/admin/model/project/project.php
@@ -4,6 +4,7 @@
 namespace app\admin\model\project;
 
 
+use app\admin\model\admin\Admin;
 use app\admin\model\BaseModel;
 use app\admin\model\ModelTrait;
 
@@ -26,7 +27,11 @@ class project extends BaseModel
         $model = new self;
         $count = self::counts($model);
         $model = $model->page((int)$where['page'],(int)$where['limit']);
-        $data = $model->select();
+        $data = $model->select()->each(function ($item)
+        {
+            $info = Admin::getAdminInfoById($item['manager']);
+            if ($info) $item['manager'] = $info['realname'];
+        });
         if ($data) $data = $data->toArray();
         return compact("data","count");
     }
