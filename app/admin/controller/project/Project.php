2313e633238682f53e06cfb11ad55001c63173dc
cfn <cfn@leapy.cn>
2020-02-24 15:58:01 +0800

    项目

--- app/admin/controller/project/Project.php
+++ app/admin/controller/project/Project.php
@@ -48,19 +48,19 @@ class project extends AuthController
     public function add(Request $request)
     {
         $form = array();
-        $form[] = Elm::input('name','登录账号')->col(10);
-        $form[] = Elm::input('nickname','昵称')->col(10);
-        $form[] = Elm::uploadImage('avatar','头像',url('/admin/widget.files/image'))->limit(1)->col(10);
-        $form[] = Elm::password('pwd','密码')->col(10);
-        $form[] = Elm::input('realname','真实姓名')->col(10);
-        $form[] = Elm::select('role_id','角色')->options(function(){
-            $list = aModel::all();
+        $form[] = Elm::input('name','项目名称')->col(10);
+        $form[] = Elm::select('manager','项目负责人')->options(function(){
+            $list = aModel::lst();
             $menus=[];
             foreach ($list as $menu){
-                $menus[] = ['value'=>$menu['id'],'label'=>$menu['name']];//,'disabled'=>$menu['pid']== 0];
+                $menus[] = ['value'=>$menu['id'],'label'=>$menu['realname']];//,'disabled'=>$menu['pid']== 0];
             }
             return $menus;
         })->col(10);
+        $form[] = Elm::input('nickname','')->col(10);
+        $form[] = Elm::password('pwd','密码')->col(10);
+        $form[] = Elm::input('realname','真实姓名')->col(10);
+
         $form[] = Elm::input('tel','电话')->col(10);
         $form[] = Elm::email('mail','邮箱')->col(10);
         $form[] = Elm::radio('status','状态',1)->options([['label'=>'启用','value'=>1],['label'=>'冻结','value'=>0]])->col(10);
