a1cead0833284d4e6e2ed66bf053cb6f85006293
cfn <cfn@leapy.cn>
2020-02-27 14:26:39 +0800

    项目

--- app/admin/controller/system/SystemConfigTab.php
+++ app/admin/controller/system/SystemConfigTab.php
@@ -6,6 +6,8 @@ namespace app\admin\controller\system;
 
 use app\admin\controller\AuthController;
 use app\Request;
+use FormBuilder\Factory\Elm;
+use learn\services\FormBuilderService as Form;
 use learn\services\JsonService as Json;
 use learn\services\UtilService as Util;
 use app\admin\model\system\SystemConfigTab as tModel;
@@ -33,5 +35,76 @@ class SystemConfigTab extends AuthController
             ['limit',20],
         ]);
         return Json::successlayui(tModel::lst($where));
+    }
+
+    /**
+     * 添加
+     * @param Request $request
+     * @return string
+     * @throws \FormBuilder\Exception\FormBuilderException
+     */
+    public function add(Request $request)
+    {
+        $form = array();
+        $form[] = Elm::input('name','分类名称')->col(10);
+        $form[] = Elm::number('rank','排序')->col(24);
+        $form[] = Elm::radio('status','状态',1)->options([['label'=>'禁用','value'=>0],['label'=>'启用','value'=>1]])->col(24);
+        return Form::make_post_form($form, url('save')->build());
+    }
+
+    /**
+     * 修改
+     * @param Request $request
+     * @return string
+     * @throws \FormBuilder\Exception\FormBuilderException
+     */
+    public function edit($id='')
+    {
+        if (!$id) return app("json")->fail("项目id不能为空");
+        $info = tModel::get($id);
+        if (!$info) return app("json")->fail("没有该项目");
+        $form = array();
+        $form[] = Elm::input('name','分类名称',$info['name'])->col(10);
+        $form[] = Elm::number('rank','排序',$info['rank'])->col(24);
+        $form[] = Elm::radio('status','状态',$info['status'])->options([['label'=>'禁用','value'=>0],['label'=>'启用','value'=>1]])->col(24);
+        return Form::make_post_form($form, url('save',["id"=>$id])->build());
+    }
+
+    /**
+     * 保存修改
+     * @param string $id
+     * @return mixed
+     */
+    public function save($id="")
+    {
+        $data = Util::postMore([
+            ['name',''],
+            ['rank',0],
+            ['status',1]
+        ]);
+        if ($data['name'] == "") return app("json")->fail("分类名称不能为空");
+        if ($id=="")
+        {
+            $data['create_user'] = $this->adminId;
+            $data['create_time'] = time();
+            $res = tModel::insert($data);
+        }else
+        {
+            $data['update_user'] = $this->adminId;
+            $data['update_time'] = time();
+            $res = tModel::update($data,['id'=>$id]);
+        }
+        return $res ? Json::success("操作成功") : app("json")->fail("操作失败");
+    }
+
+    /**
+     * 删除
+     * @param $id
+     * @return
+     */
+    public function del($id)
+    {
+        if (!$id) return app("json")->fail("参数有误，Id为空！");
+        return tModel::del($id) ? app("json")->success("操作成功") : app("json")->fail("操作失败");
     }
 }
