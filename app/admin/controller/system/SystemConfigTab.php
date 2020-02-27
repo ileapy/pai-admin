8035970d82d0ea08acfc5f3a325a820d3afde167
cfn <cfn@leapy.cn>
2020-02-27 14:19:43 +0800

    项目

--- app/admin/controller/system/SystemConfigTab.php
+++ app/admin/controller/system/SystemConfigTab.php
@@ -5,11 +5,34 @@ namespace app\admin\controller\system;
 
 
 use app\admin\controller\AuthController;
+use app\Request;
+use learn\services\JsonService as Json;
+use learn\services\UtilService as Util;
+use app\admin\model\system\SystemConfigTab as tModel;
 
+/**
+ * 管理员配置
+ * Class SystemConfigTab
+ * @package app\admin\controller\system
+ */
 class SystemConfigTab extends AuthController
 {
     public function index()
     {
         return $this->fetch();
+    }
+
+    /**
+     * 列表
+     * @param Request $request
+     * @return mixed
+     */
+    public function lst(Request $request)
+    {
+        $where = Util::postMore([
+            ['page',1],
+            ['limit',20],
+        ]);
+        return Json::successlayui(tModel::lst($where));
     }
 }
--- app/admin/model/system/SystemConfigTab.php
+++ app/admin/model/system/SystemConfigTab.php
@@ -10,4 +10,19 @@ use app\admin\model\ModelTrait;
 class SystemConfigTab extends BaseModel
 {
     use ModelTrait;
+
+    /**
+     * 列表
+     * @param $where
+     * @return array
+     */
+    public static function lst($where)
+    {
+        $model = new self;
+        $count = self::counts($model);
+        $model = $model->page((int)$where['page'],(int)$where['limit']);
+        $data = $model->select();
+        if ($data) $data = $data->toArray();
+        return compact("data","count");
+    }
 }
