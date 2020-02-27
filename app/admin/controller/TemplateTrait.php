84f3cb10f23b914baa7289ac0bed325539845df3
cfn <cfn@leapy.cn>
2020-02-27 17:51:20 +0800

    项目

--- app/admin/controller/TemplateTrait.php
+++ app/admin/controller/TemplateTrait.php
@@ -3,16 +3,20 @@
 
 namespace app\admin\controller;
 
-
+/**
+ * Trait TemplateTrait
+ * @package app\admin\controller
+ */
 trait TemplateTrait
 {
     /**
      * 删除操作
      * @param $id
-     * @return
+     * @return mixed
      */
-    public static function del($id)
+    public function del($id)
     {
+        var_dump($id);
         if (!$id) return app("json")->fail("参数有误，Id为空！");
         return self::del($id) ? app("json")->success("操作成功") : app("json")->fail("操作失败");
     }
