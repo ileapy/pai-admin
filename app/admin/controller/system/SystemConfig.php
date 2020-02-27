f578698caf2381fd59f628747336d49a317e6331
cfn <cfn@leapy.cn>
2020-02-27 13:40:23 +0800

    项目

--- app/admin/controller/system/SystemConfig.php
+++ app/admin/controller/system/SystemConfig.php
@@ -13,7 +13,42 @@ use app\admin\controller\AuthController;
  */
 class SystemConfig extends AuthController
 {
-    public function index()
+    /**
+     * 基础配置
+     * @return string
+     * @throws \Exception
+     */
+    public function base()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 上传配置
+     * @return string
+     * @throws \Exception
+     */
+    public function upload()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     * 短信配置
+     * @return string
+     * @throws \Exception
+     */
+    public function sms()
+    {
+        return $this->fetch();
+    }
+
+    /**
+     *邮件配置
+     * @return string
+     * @throws \Exception
+     */
+    public function email()
     {
         return $this->fetch();
     }
