96efe76612f7e07e892a27ddef1687149274d16e
cfn <cfn@leapy.cn>
2020-02-27 19:07:56 +0800

    项目

--- app/admin/controller/TemplateTrait.php
+++ app/admin/controller/TemplateTrait.php
@@ -14,10 +14,34 @@ trait TemplateTrait
      * @param $id
      * @return mixed
      */
-    public function del($ids)
+    public function del($ids = 0)
     {
         if (empty($ids) || !$ids) return app("json")->fail("参数有误，Id为空！");
         if (!is_array($ids)) $ids = [$ids];
         return $this->model->where("id","in",$ids)->delete() ? app("json")->success("操作成功") : app("json")->fail("操作失败");
+    }
+
+    /**
+     * 启用
+     * @param int $ids
+     * @return
+     */
+    public function enabled($ids = 0)
+    {
+        if (empty($ids) || !$ids) return app("json")->fail("参数有误，Id为空！");
+        if (!is_array($ids)) $ids = [$ids];
+        return $this->model->where("id","in",$ids)->update(['status'=>1]) ? app("json")->success("操作成功") : app("json")->fail("操作失败");
+    }
+
+    /**
+     * 禁用
+     * @param int $ids
+     * @return
+     */
+    public function disabled($ids = 0)
+    {
+        if (empty($ids) || !$ids) return app("json")->fail("参数有误，Id为空！");
+        if (!is_array($ids)) $ids = [$ids];
+        return $this->model->where("id","in",$ids)->update(['status'=>0]) ? app("json")->success("操作成功") : app("json")->fail("操作失败");
     }
 }
