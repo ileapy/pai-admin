1d7304b164b6b6466c2f7d2b7f30f106ba06fed0
cfn <cfn@leapy.cn>
2020-02-27 19:13:25 +0800

    项目

--- app/admin/controller/TemplateTrait.php
+++ app/admin/controller/TemplateTrait.php
@@ -16,7 +16,7 @@ trait TemplateTrait
      */
     public function del($ids = 0)
     {
-        if (empty($ids) || !$ids) return app("json")->fail("参数有误，Id为空！");
+        if ($ids == 0) return app("json")->fail("参数有误，Id为空！");
         if (!is_array($ids)) $ids = [$ids];
         return $this->model->where("id","in",$ids)->delete() ? app("json")->success("操作成功") : app("json")->fail("操作失败");
     }
