3c376f98a5f4a4f7557ef44bb664d731989b1b1b
cfn <cfn@leapy.cn>
2020-03-02 16:21:08 +0800

    项目

---
+++ app/admin/controller/Test.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace app\admin\controller;
+
+use learn\services\sms\QCloudSmsService;
+
+class Test extends AuthController
+{
+    // 无需登录的
+    protected $noNeedLogin = [];
+    // 无需权限的
+    protected $noNeedRight = [];
+
+    /**
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function index()
+    {
+        $sms = new QCloudSmsService(systemConfig("sms_appid"),systemConfig("sms_appkey"));
+        $sms->setPhoneNumbers(['18438622618']);
+        var_dump($sms->sendSingleSms(systemConfig("sms_login"),['123456'],systemConfig("sms_sign")));
+    }
+
+}
