4b93d518ca47318179510b75386f19ed796360c1
cfn <cfn@leapy.cn>
2020-03-02 16:12:10 +0800

    项目

--- learn/services/sms/QCloudSmsService.php
+++ learn/services/sms/QCloudSmsService.php
@@ -5,6 +5,8 @@ namespace learn\services\sms;
 
 use Qcloud\Sms\SmsMultiSender;
 use Qcloud\Sms\SmsSingleSender;
+use Qcloud\Sms\SmsVoicePromptSender;
+use Qcloud\Sms\SmsVoiceVerifyCodeSender;
 
 class QCloudSmsService
 {
@@ -69,6 +71,32 @@ class QCloudSmsService
     {
         $this->smSender = new SmsMultiSender($this->appId,$this->appKey);
         $res = $this->smSender->sendWithParam("86", $this->phoneNumbers, $templId, $params, $sign, "", "");
+        var_dump($res);
+        return true;
+    }
+
+    /**
+     * 发送语言短信验证码
+     * @param string $verifyCode
+     * @return bool
+     */
+    public function sendVoiceVerifySms(string $verifyCode)
+    {
+        $this->smSender = new SmsVoiceVerifyCodeSender($this->appId,$this->appKey);
+        $res = $this->smSender->send("86", $this->phoneNumbers[0], $verifyCode);
+        var_dump($res);
+        return true;
+    }
+
+    /**
+     * 发送语言通知
+     * @param string $msg
+     * @return bool
+     */
+    public function sendVoicePromptSms(string $msg)
+    {
+        $this->smSender = new SmsVoicePromptSender($this->appId,$this->appKey);
+        $res = $this->smSender->send("86", $this->phoneNumbers[0], 2, $msg);
         var_dump($res);
         return true;
     }
