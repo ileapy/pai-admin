cbd16552cd084c75da54c2b5c20e8f993c50fc21
cfn <cfn@leapy.cn>
2020-03-04 00:38:00 +0800

    微信公众号

--- learn/subscribes/WechatMessageSubscribe.php
+++ learn/subscribes/WechatMessageSubscribe.php
@@ -18,7 +18,7 @@ class WechatMessageSubscribe
     public function onMessageBefore($event)
     {
         list($message) = $event;
-        file_put_contents("db.log", json_encode($message,true));
+        file_put_contents("db.log", $message->MsgType);
         $event = isset($message->MsgType) && strtolower($message->MsgType) == 'event' ? strtolower($message->Event) : strtolower($message->MsgType) ;
         file_put_contents("da.log", $event);
         WechatMessage::saveMessage($message->FromUserName, $event, json_encode($message,true));
