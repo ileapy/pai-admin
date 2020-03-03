56fae7d894a2fa0470d7a6f15b4c7c7ea3dfc056
cfn <cfn@leapy.cn>
2020-03-03 17:03:38 +0800

    定时器

--- learn/services/WechatService.php
+++ learn/services/WechatService.php
@@ -3,8 +3,8 @@
 
 namespace learn\services;
 
-use app\admin\model\system\SystemConfig;
 use EasyWeChat\Work\Application;
+use EasyWeChat\Work\Server\Guard;
 use think\Response;
 
 /**
@@ -21,14 +21,16 @@ class WechatService
     private static $instance = null;
 
     /**
-     * 配置
+     * 配置项
      * @return array
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
      */
     public static function options()
     {
         $wechat = SystemConfigMore(['wechat_appid','wechat_appsecret','wechat_token','wechat_aeskey','wechat_encry']);
-        var_dump($wechat);
-        $payment = SystemConfigMore(['pay_weixin_mchid','pay_weixin_client_cert','pay_weixin_client_key','pay_weixin_key','pay_weixin_open']);
+        //$payment = SystemConfigMore(['pay_weixin_mchid','pay_weixin_client_cert','pay_weixin_client_key','pay_weixin_key','pay_weixin_open']);
         $config = [
             'app_id'=>isset($wechat['wechat_appid']) ? trim($wechat['wechat_appid']) :'',
             'secret'=>isset($wechat['wechat_appsecret']) ? trim($wechat['wechat_appsecret']) :'',
@@ -37,29 +39,42 @@ class WechatService
                 'timeout' => 10.0, // 超时时间（秒）
             ],
         ];
-        if(isset($wechat['wechat_encode']) && (int)$wechat['wechat_encode']>0 && isset($wechat['wechat_encodingaeskey']) && !empty($wechat['wechat_encodingaeskey']))
-            $config['aes_key'] =  $wechat['wechat_encodingaeskey'];
-        if(isset($payment['pay_weixin_open']) && $payment['pay_weixin_open'] == 1){
-            $config['payment'] = [
-                'merchant_id'=>trim($payment['pay_weixin_mchid']),
-                'key'=>trim($payment['pay_weixin_key']),
-                'cert_path'=>realpath('.'.$payment['pay_weixin_client_cert']),
-                'key_path'=>realpath('.'.$payment['pay_weixin_client_key']),
-                //'notify_url'=>SystemConfigService::get('site_url').Url::buildUrl('wap/Wechat/notify')
-                'notify_url'=>SystemConfigService::get('site_url') . '/api/wechat/notify'
-            ];
-        }
+        if(isset($wechat['wechat_encry']) && (int)$wechat['wechat_encry']>1 && isset($wechat['wechat_aeskey']) && !empty($wechat['wechat_aeskey']))
+            $config['aes_key'] =  $wechat['wechat_aeskey'];
+//        if(isset($payment['pay_weixin_open']) && $payment['pay_weixin_open'] == 1){
+//            $config['payment'] = [
+//                'merchant_id'=>trim($payment['pay_weixin_mchid']),
+//                'key'=>trim($payment['pay_weixin_key']),
+//                'cert_path'=>realpath('.'.$payment['pay_weixin_client_cert']),
+//                'key_path'=>realpath('.'.$payment['pay_weixin_client_key']),
+//                //'notify_url'=>SystemConfigService::get('site_url').Url::buildUrl('wap/Wechat/notify')
+//                'notify_url'=>SystemConfigService::get('site_url') . '/api/wechat/notify'
+//            ];
+//        }
         return $config;
     }
 
-
-
+    /**
+     * 应用
+     * @param bool $cache
+     * @return Application|null
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
     public static function application($cache = false)
     {
         (self::$instance === null || $cache === true) && (self::$instance = new Application(self::options()));
         return self::$instance;
     }
 
+    /**
+     * 服务
+     * @return Response
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
     public static function serve():Response
     {
         $wechat = self::application(true);
