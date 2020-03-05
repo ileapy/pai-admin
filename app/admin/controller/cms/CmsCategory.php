07acab29e87e7b4d3a5e15c0069468ed6fe53a1d
cfn <cfn@leapy.cn>
2020-03-05 23:10:56 +0800

    builder

---
+++ app/admin/controller/cms/CmsCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace app\admin\controller\cms;
+
+
+use app\admin\controller\AuthController;
+
+/**
+ * Class CmsCategory
+ * @package app\admin\controller\cms
+ */
+class CmsCategory extends AuthController
+{
+
+}
---
+++ app/admin/model/cms/CmsCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\admin\model\cms;
+
+
+use app\admin\model\BaseModel;
+use app\admin\model\ModelTrait;
+
+/**
+ * Class CmsCategory
+ * @package app\admin\model\cms
+ */
+class CmsCategory extends BaseModel
+{
+    use ModelTrait;
+}
