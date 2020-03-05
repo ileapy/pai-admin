1b90fabd7371e56f1bcf339acf3e0379961155e7
cfn <cfn@leapy.cn>
2020-03-05 23:19:24 +0800

    cms

--- app/admin/controller/cms/CmsCategory.php
+++ app/admin/controller/cms/CmsCategory.php
@@ -5,6 +5,12 @@ namespace app\admin\controller\cms;
 
 
 use app\admin\controller\AuthController;
+use app\Request;
+use learn\services\UtilService as Util;
+use learn\services\JsonService as Json;
+use FormBuilder\Factory\Elm;
+use learn\services\FormBuilderService as Form;
+use app\admin\model\cms\CmsCategory as CModel;
 
 /**
  * Class CmsCategory
@@ -12,5 +18,28 @@ use app\admin\controller\AuthController;
  */
 class CmsCategory extends AuthController
 {
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    public function index()
+    {
+        return $this->fetch();
+    }
 
+    /**
+     * 列表
+     * @param Request $request
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public function lst(Request $request)
+    {
+        $where = Util::postMore([
+            ['name',''],
+            ['status',''],
+        ]);
+        return Json::successlayui(CModel::systemPage($where));
+    }
 }
--- app/admin/model/cms/CmsCategory.php
+++ app/admin/model/cms/CmsCategory.php
@@ -14,4 +14,21 @@ use app\admin\model\ModelTrait;
 class CmsCategory extends BaseModel
 {
     use ModelTrait;
+
+    /**
+     * 列表
+     * @param $where
+     * @return array
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
+    public static function systemPage($where)
+    {
+        $model = new self;
+        if ($where['name']) $model = $model->where("name","like", $where['name']);
+        if ($where['status']) $model = $model->where("status", $where['status']);
+        $data = $model->select();
+        return $data ? $data->toArray() : [];
+    }
 }
