a7f179a74787b1184655408e44ff08ca0962f88a
cfn <cfn@leapy.cn>
2020-03-05 15:15:02 +0800

    builder

---
+++ app/admin/controller/database/Redis.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace app\admin\controller\database;
+
+
+use app\admin\controller\AuthController;
+
+
+/**
+ * Class Redis
+ * @package app\admin\controller\database
+ */
+class Redis extends AuthController
+{
+
+    public function index()
+    {
+        $redis = app("redis");
+        var_dump($redis->keys('*'););
+    }
+}
