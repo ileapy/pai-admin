814d2875d738c4ab7d5ab80778186b036889da78
cfn <cfn@leapy.cn>
2020-03-05 15:42:18 +0800

    builder

--- app/admin/controller/database/Redis.php
+++ app/admin/controller/database/Redis.php
@@ -27,4 +27,22 @@ class Redis extends AuthController
         $this->assign("keys",$redis->keys('*'));
         return $this->fetch();
     }
+
+    /**
+     * 详细
+     * @param string $key_name
+     * @return string
+     * @throws \Exception
+     */
+    public function detail($key_name="")
+    {
+        if (!$key_name) return "KEY为空";
+        $redis = app("redis");
+        $redis->connect(config("cache.stores.redis.host"),config("cache.stores.redis.port"));
+        if (config("cache.stores.redis.password")) $redis->auth(config("cache.stores.redis.password"));
+        $redis->select(config("cache.stores.redis.select"));
+        $this->assign("key_name",$key_name);
+        $this->assign("value",$redis->get($key_name));
+        return $this->fetch();
+    }
 }
