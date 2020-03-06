805964c2b90b4d979ca1d4d9618c8f4feadefb09
cfn <cfn@leapy.cn>
2020-03-07 00:11:15 +0800

    cms

---
+++ app/admin/controller/cms/CmsTag.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\admin\controller\cms;
+
+
+use app\admin\controller\AuthController;
+
+/**
+ * 文章标签
+ * Class CmsTag
+ * @package app\admin\controller\cms
+ */
+class CmsTag extends AuthController
+{
+
+}
