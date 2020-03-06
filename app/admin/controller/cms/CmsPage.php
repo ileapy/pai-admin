7a05415aeeab1559f6ebb13eda34a8b470e181ea
cfn <cfn@leapy.cn>
2020-03-06 14:10:45 +0800

    cms

--- app/admin/controller/cms/CmsPage.php
+++ app/admin/controller/cms/CmsPage.php
@@ -35,15 +35,33 @@ class CmsPage extends AuthController
         return $this->fetch();
     }
 
-
+    /**
+     * 添加
+     * @param Request $request
+     * @return string
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
     public function add(Request $request)
     {
-
+        $this->assign("category",CModel::selectByType(1));
+        return $this->fetch();
     }
 
+    /**
+     * 修改
+     * @param Request $request
+     * @return string
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\DbException
+     * @throws \think\db\exception\ModelNotFoundException
+     */
     public function edit(Request $request)
     {
-
+        $this->assign("category",CModel::selectByType(1));
+        $this->assign("info",PModel::get($request->param(['id'])));
+        return $this->fetch();
     }
 
     public function save(Request $request)
