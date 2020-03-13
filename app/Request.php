d8e9fe1d819302b88eb254cac191e97264e748c2
cfn <cfn@leapy.cn>
2020-03-13 14:13:47 +0800

    宏指令，验证器

--- app/Request.php
+++ app/Request.php
@@ -2,7 +2,14 @@
 namespace app;
 
 // 应用请求对象类
+use Spatie\Macroable\Macroable;
+
+/**
+ * Class Request
+ * @package app
+ */
 class Request extends \think\Request
 {
-
+    // 宏指令
+    use Macroable;
 }
