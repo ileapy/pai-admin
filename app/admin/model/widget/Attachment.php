ead285aa9c12bd2b175d0e031d15fa45779660f6
cfn <cfn@leapy.cn>
2020-04-07 16:07:19 +0800

    数据库备份+附件管理

--- app/admin/model/widget/Attachment.php
+++ app/admin/model/widget/Attachment.php
@@ -6,6 +6,7 @@ namespace app\admin\model\widget;
 
 use app\admin\model\BaseModel;
 use app\admin\model\ModelTrait;
+use app\admin\model\system\SystemConfig;
 
 /**
  * Class attachment
@@ -14,4 +15,19 @@ use app\admin\model\ModelTrait;
 class Attachment extends BaseModel
 {
     use ModelTrait;
+
+    /**
+     * @param int $cid
+     * @param string $name
+     * @param string $path
+     * @param string $type
+     * @param string $mime
+     * @param float $size
+     */
+    public static function add(int $cid, string $name, string $path, string $type, string $mime, float $size)
+    {
+        // 存储方式
+        $storage = SystemConfig::get("");
+        $upload_time = time();
+    }
 }
