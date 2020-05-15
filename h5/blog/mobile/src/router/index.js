9c6b394aab57b5eed2f5f112b1dbde80c7ca24be
cfn <cfn@leapy.cn>
2020-05-15 19:01:42 +0800

    博客

--- h5/blog/mobile/src/router/index.js
+++ h5/blog/mobile/src/router/index.js
@@ -8,7 +8,8 @@ Vue.use(VueRouter)
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta:{title:'首页',keyword:'首页',description:'首页'},
   },
   {
     path: '/about',
@@ -21,7 +22,27 @@ Vue.use(VueRouter)
 ]
 
 const router = new VueRouter({
+  mode:'history',
   routes
+})
+
+// 修改页面title
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+
+  if(to.meta){
+    let head = document.getElementsByTagName('head');
+    let meta = document.createElement('meta');
+    document.querySelector('meta[name="keywords"]').setAttribute('content', to.meta.keywords)
+    document.querySelector('meta[name="description"]').setAttribute('content', to.meta.description)
+    meta.content = to.meta.content;
+    head[0].appendChild(meta)
+  }
+
+  next()
 })
 
 export default router
