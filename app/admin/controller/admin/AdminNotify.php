b7f5ca8079a598e5344f9e89c16bb56bdf82889f
cfn <cfn@leapy.cn>
2020-05-26 09:43:22 +0800

    消息通知

---
+++ app/admin/controller/admin/AdminNotify.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\admin\controller\admin;
+
+
+use app\admin\controller\AuthController;
+
+/**
+ * 消息提醒
+ * Class AdminNotify
+ * @package app\admin\controller\admin
+ */
+class AdminNotify extends AuthController
+{
+
+}
---
+++ app/admin/model/admin/AdminNotify.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace app\admin\model\admin;
+
+
+use app\admin\model\BaseModel;
+
+/**
+ * 消息通知
+ * Class AdminNotify
+ * @package app\admin\model\admin
+ */
+class AdminNotify extends BaseModel
+{
+
+}
