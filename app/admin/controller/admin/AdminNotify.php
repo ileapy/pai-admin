5257e962014cebafd88fbfb25997ab208f58ca25
cfn <cfn@leapy.cn>
2020-05-26 11:28:05 +0800

    消息通知

--- app/admin/controller/admin/AdminNotify.php
+++ app/admin/controller/admin/AdminNotify.php
@@ -5,7 +5,8 @@ namespace app\admin\controller\admin;
 
 
 use app\admin\controller\AuthController;
-
+use learn\services\UtilService as Util;
+use app\admin\model\admin\AdminNotify as nModel;
 /**
  * 消息提醒
  * Class AdminNotify
@@ -13,5 +14,17 @@ use app\admin\controller\AuthController;
  */
 class AdminNotify extends AuthController
 {
-
+    /**
+     * 列表
+     * @return mixed
+     */
+    public function lst()
+    {
+        $where = Util::postMore([
+            ['is_read',''],
+            ['page',1],
+            ['limit',20],
+        ]);
+        return app("json")->layui(nModel::systemPage($where));
+    }
 }
--- app/admin/model/admin/AdminNotify.php
+++ app/admin/model/admin/AdminNotify.php
@@ -13,5 +13,18 @@ use app\admin\model\BaseModel;
  */
 class AdminNotify extends BaseModel
 {
-
+    /**
+     * 系统分页
+     * @param array $where
+     * @return array
+     */
+    public static function systemPage(array $where)
+    {
+        $model = new self;
+        if ($where['is_read'] != '') $model = $model->where("is_read",$where['is_read']);
+        $model = $model->order("add_time desc");
+        $model = $model->page((int)$where['page'], (int)$where['limit']);
+        $data = $model->select();
+        return $data ? $data->toArray() : [];
+    }
 }
