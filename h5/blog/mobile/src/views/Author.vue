d3e3acd5adcc193f09d6241d2a0e2f9e29006aa8
cfn <cfn@leapy.cn>
2020-05-29 11:40:06 +0800

    公众号授权登录，跳转到原页面

--- h5/blog/mobile/src/views/Author.vue
+++ h5/blog/mobile/src/views/Author.vue
@@ -1,21 +1,52 @@
 <template>
     <div>
-        <van-button round block type="info" native-type="submit" @click="login">
-            授权登录
-        </van-button>
+        <van-form @submit="onSubmit">
+            <van-field
+                    v-model="tel"
+                    label-align="right"
+                    label="手机号"
+                    placeholder="手机号"
+                    :rules="[{ pattern : /^1(3|4|5|6|7|8|9)\d{9}$/,message: '手机号填写有误！'}]"
+            />
+            <van-field
+                    v-model="sms"
+                    label-align="right"
+                    center
+                    clearable
+                    label="短信验证码"
+                    :rules="[{ required: true }]"
+            >
+                <template #button>
+                    <van-button size="small" type="primary" native-type="button">发送验证码</van-button>
+                </template>
+            </van-field>
+            <div style="margin: 16px;">
+                <van-button round block type="info" native-type="submit">
+                    登录
+                </van-button>
+            </div>
+        </van-form>
     </div>
 </template>
 
 <script>
+    import Vue from 'vue';
     import store from '../store'
     import {_URL,parseQuery} from "../utils";
     import cookie from "../utils/cookie";
     import router from "../router";
+    import { Form } from 'vant';
+    import { Field } from 'vant';
+
+    Vue.use(Field);
+    Vue.use(Form);
 
     export default {
         data(){
             return {
-                loading:false
+                loading:false,
+                tel: '',
+                sms: '',
             };
         },
         mounted() {
@@ -36,9 +67,10 @@
             }
         },
         methods: {
-            login(){
-                console.log(111)
-            }
+            onSubmit(values) {
+                console.log('tel', this.tel);
+                console.log('sms', this.sms);
+            },
         }
     }
 </script>
